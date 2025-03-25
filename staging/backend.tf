4c7cc08dc81423b9d04cb488ee21295da72f45f9
Danish A M <41690164+danish-am@users.noreply.github.com>
2025-03-25 15:58:08 -0400

    Update backend.tf

--- staging/backend.tf
+++ staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "terraformadodanish"
+    storage_account_name = "tfstagedanish"
     container_name      = "tfstate"
     key                 = "stage.tfstate"
   }
