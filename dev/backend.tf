bab11ba20b03a6689758d8ff6598d1829090b1db
Danish A M <41690164+danish-am@users.noreply.github.com>
2025-03-25 15:58:40 -0400

    Update backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
-    storage_account_name = "devterraformadodanish"
+    storage_account_name = "tfdevbackdanish"
     container_name      = "tfstate"
     key                 = "dev.tfstate"
   }
